d9d327770ad1e70b7662627b0c636d2d681ab316
alamin <alaminjoy456@gmail.com>
2024-09-09 06:31:19 +0600

    14 done

---
+++ src/1013-The Greatest.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+import kotlin.math.abs
+fun main(args: Array<String>) {
+    val input = Scanner(System.`in`)
+    val a = input.nextInt()
+    val b = input.nextInt()
+    val s = input.nextInt()
+
+
+}
---
+++ src/1014-Consumption.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main(){
+    val input= Scanner(System.`in`)
+    val X=input.nextInt()
+    val Y=input.nextFloat()
+    val average=X/Y
+    print("%.3f km/1".format(average))
+
+
+}
